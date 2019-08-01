2ecc5771168a1f9b17b3d3e8c600b8c09dd52042
Ryan Swiatnik <swiryan@gmail.com>
2019-08-01 20:41:35 +0930

    Added basic server auto saving functionality

---
+++ dbzdnd/dbzdnd/Network.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace dbzdnd
+{
+    public class Network
+    {
+        private readonly string ip;
+        private readonly int port;
+
+        public Network(string ip, int port)
+        {
+            this.ip = ip;
+            this.port = port;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Auto Saving");
+            try
+            {
+                //Create network client
+                TcpClient client = new TcpClient(ip, port);
+                StreamWriter sr = new StreamWriter(client.GetStream());
+
+                //Write save string here
+                sr.WriteLine("It Worked!");
+                sr.Flush();
+
+                Console.WriteLine("Saved");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+    }
+}
---
+++ dndServer/dndServer/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace dndServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting D&D save server");
+            
+            TcpListener server = null;
+
+            try
+            {
+                //Start server
+                server = new TcpListener(IPAddress.Parse("0.0.0.0"), 25000);
+                server.Start();
+
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                //Listen loop
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+
+                    StreamReader sr = new StreamReader(client.GetStream());
+
+                    //Get file string
+                    data = sr.ReadLine();
+
+
+                    if (data.StartsWith("GET"))
+                    {
+                        //Get save file
+                    } else
+                    {
+                        //Save, save file
+                        System.IO.File.WriteAllText("test.txt", data);
+                        Console.WriteLine("File saved from: " + client.Client.RemoteEndPoint.ToString());
+                    }
+                    
+                    //Console.WriteLine(data);
+                    
+                    client.Close();
+                }
+
+            } catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            } finally
+            {
+                server.Stop();
+            }
+            
+        }
+    }
+}
