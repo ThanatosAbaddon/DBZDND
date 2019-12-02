a3cc12d29de5818e331301519cf443481c7c7364
Ryan Swiatnik <swira001@mymail.unisa.edu.au>
2019-12-02 18:32:22 +1030

    Removed local image resource.

--- dbzdnd/dbzdnd/Properties/Resources.Designer.cs
+++ dbzdnd/dbzdnd/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace dbzdnd.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap CharacterPic {
-            get {
-                object obj = ResourceManager.GetObject("CharacterPic", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
